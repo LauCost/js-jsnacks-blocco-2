5558fc1b59cf8d41ab0d60958ec79120430bce03
Laurentiu <lcostin40@gmail.com>
2021-10-25 12:21:11 +0200

    Creato arrey di oggetti

---
+++ JSnack 2A/main.js
@@ -0,0 +1,52 @@
+const squadre = [
+
+    {
+
+        nome: "Inter",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+    {
+        nome: "Roma",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+    {
+        nome: "Napoli",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+    {
+        nome: "Verona",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+    {
+        nome: "Torino",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+    {
+        nome: "Lazio",
+
+        puntiFatti: 0,
+
+        falliSubiti: 0
+    },
+
+]
