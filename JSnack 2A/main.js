8b4fb2245e67a4f286eccb3dd80c4ec04ae72ae5
Laurentiu <lcostin40@gmail.com>
2021-10-25 12:45:55 +0200

    Aggiunto js per calcolare numeri random per falli e punti

--- JSnack 2A/main.js
+++ JSnack 2A/main.js
@@ -50,3 +50,19 @@ const squadre = [
     },
 
 ]
+
+
+for (let i = 0; i < squadre.length; i++) {
+    const club = squadre[i];
+    club.puntiFatti = Math.floor(Math.random() * 10);
+    club.falliSubiti = Math.floor(Math.random() * 10);
+
+
+}
+
+for (const key in squadre) {
+
+    const element = squadre[key];
+
+    console.log(squadre[key]);
+}
