f809481fc78a89a424bb02dd9e45666568b5a498
Laurentiu <lcostin40@gmail.com>
2021-10-26 12:38:10 +0200

    Creato index e js con array di oggetti

--- JSnack3/main.js
+++ JSnack3/main.js
@@ -1,3 +1,17 @@
 
 let numeri = [];
 
+somma = 0;
+
+while (somma < 50) {
+
+    let numero = parseInt(prompt("inserisci un numero"));
+
+    numeri.push(numero);
+
+    somma += numero;
+
+    console.log(somma);
+}
+
+console.log(numeri);
---
+++ JSnack4/mian.js
@@ -0,0 +1,22 @@
+const bike = [
+
+    {
+
+        nome: "Mountain bike",
+
+        peso: 10
+    },
+
+    {
+        nome: "Bici da corsa",
+
+        peso: 8
+    },
+
+    {
+        nome: "Bici da cicloturismo",
+
+        peso: 9
+    }
+
+]
